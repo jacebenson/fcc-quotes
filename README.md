f962ef0d26d1f9401ece2f836f3bd8c954729543
Jace Benson <jace.benson@gmail.com>
2018-06-13 20:54:28 -0500

    added tests

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
-# Quotes
+# Quote Machine
 
 This frontend project is to use the skills learned at FreeCodeCamp.
 
-The stories from this project are listed on [FreeCodeCamp](https://www.freecodecamp.org/challenges/build-a-random-quote-machine).
+The stories from this project are listed on [FreeCodeCamp](https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-drum-machine/).
 
 Below I've listed them incase that changes;
 
-1. User Story: I can click a button to show me a new random quote.
-1. User Story: I can press a button to tweet out a quote.
-   
+1. User Story #1: I can see a wrapper element with a corresponding `id="quote-box"`.
+1. User Story #2: Within `#quote-box`, I can see an element with a corresponding `id="text"`.
+1. User Story #3: Within `#quote-box`, I can see an element with a corresponding `id="author"`.
+1. User Story #4: Within `#quote-box`, I can see a clickable element with a corresponding id="new-quote".
+1. User Story #5: Within `#quote-box`, I can see a clickable element with a corresponding id="tweet-quote".
+1. User Story #6: On first load, my quote machine displays a random quote in the element with id="text".
+1. User Story #7: On first load, my quote machine displays the random quote's author in the element with id="author".
+1. User Story #8: When the `#new-quote` button is clicked, my quote machine should fetch a new quote and display it in the `#text` element.
+1. User Story #9: My quote machine should fetch the new quote's author when the #new-quote button is clicked and display it in the `#author` element.
+1. User Story #10: I can tweet the current quote by clicking on the #tweet-quote a element. This a element should include the "twitter.com/intent/tweet" path in it's href attribute to tweet the current quote.
+1. User Story #11: The `#quote-box` wrapper element should be horizontally centered. Please run tests with browser's zoom level at 100% and page maximized.
+
 ## Getting Started
 
 These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
@@ -23,18 +32,21 @@ These instructions will get you a copy of the project up and running on your loc
 A step by step series of examples that tell you have to get a development env running
 
 Clone the repo, `https://gitlab.com/jacebenson/fcc-quotes.git`
-```
+
+```sh
 git clone https://gitlab.com/jacebenson/fcc-quotes.git
 ```
 
 Install the required dependencies
-```
-cd fcc-quotes
+
+```sh
+cd fcc-survey
 npm install
 ```
 
 To get this repo hosting I've included a npm script to spin up a server with this command;
-```
+
+```sh
 npm start
 ```
 
@@ -55,7 +67,6 @@ So all that's needed is the content of the page needs to exist in `/public`.
 
 I don't know why'd you'd want to contribute but if you do or want to see if the pull is wanted open a Issue.
 
-
 ## Authors
 
-* **Jace Benson** 
+Jace Benson
--- public/custom.js
+++ public/custom.js
@@ -1,94 +1,107 @@
-var quotes = [
+var GLOBAL = {};
+GLOBAL.quoteUsed = '';
+GLOBAL.quote = '';
+GLOBAL.quotes = [
   {
-    author:"O",
-    quote:"They are kind and lots of fun."
-  },
-  {
-    author:"Daniel Tiger",
-    quote:"Won't you be my neighbor?"
-  },
-  {
-    author:"Mr. Mcfeely",
-    quote:"Speedy Delivery"
+    author: "O",
+    quote: "They are kind and lots of fun."
   },
   {
     author: "Daniel Tiger",
-    quote:  "Everyone's job is important.  We all help in different ways."
+    quote: "Won't you be my neighbor?"
+  },
+  {
+    author: "Mr. Mcfeely",
+    quote: "Speedy Delivery"
   },
   {
     author: "Daniel Tiger",
-    quote:  "If you’re feeling frustrated, take a step back, and ask for help."
+    quote: "Everyone's job is important.  We all help in different ways."
   },
   {
     author: "Daniel Tiger",
-    quote:  "When you feel jealous talk about it, and we’ll figure something out."
-  },  {
-    author: "Daniel Tiger",
-    quote:  "When you’re sick, rest is best."
+    quote: "If you’re feeling frustrated, take a step back, and ask for help."
   },
   {
     author: "Daniel Tiger",
-    quote:  "Keep trying, you’ll get better."
-  }
-  ,
-  {
+    quote: "When you feel jealous talk about it, and we’ll figure something out."
+  }, {
     author: "Daniel Tiger",
-    quote:  "Find your own way to say I love you."
-  }
-  ,
-  {
-    author: "Daniel Tiger",
-    quote:  "I can be anybody, anybody that I choose."
-  }
-  ,
-  {
-    author: "Daniel Tiger",
-    quote:  "If something seems hard to do, try it a little bit at a time."
-  }
-  ,
-  {
-    author: "Daniel Tiger",
-    quote:  "See what it is, you might feel better."
-  }
-  ,
-  {
-    author: "Daniel Tiger",
-    quote:  "Think about how someone else is feeling, maybe you can help them feel better."
-  }
-  ,
-  {
-    author: "Daniel Tiger",
-    quote:  "Close your eyes and think of something happy, if you think about something great you won’t be as afraid."
-  }
-  ,
-  {
-    author: "Daniel Tiger",
-    quote:  "When something seems bad, turn it around, and find something good."
-  }
-  ,
-  {
-    author: "Daniel Tiger",
-    quote:  "When you feel so mad that you want to roar, take a deep breath, and count to four."
+    quote: "When you’re sick, rest is best."
   },
   {
     author: "Daniel Tiger",
-    quote:  "Saying I’m sorry is the first step, then, how can I help?"
+    quote: "Keep trying, you’ll get better."
+  }
+  ,
+  {
+    author: "Daniel Tiger",
+    quote: "Find your own way to say I love you."
+  }
+  ,
+  {
+    author: "Daniel Tiger",
+    quote: "I can be anybody, anybody that I choose."
+  }
+  ,
+  {
+    author: "Daniel Tiger",
+    quote: "If something seems hard to do, try it a little bit at a time."
+  }
+  ,
+  {
+    author: "Daniel Tiger",
+    quote: "See what it is, you might feel better."
+  }
+  ,
+  {
+    author: "Daniel Tiger",
+    quote: "Think about how someone else is feeling, maybe you can help them feel better."
+  }
+  ,
+  {
+    author: "Daniel Tiger",
+    quote: "Close your eyes and think of something happy, if you think about something great you won’t be as afraid."
+  }
+  ,
+  {
+    author: "Daniel Tiger",
+    quote: "When something seems bad, turn it around, and find something good."
+  }
+  ,
+  {
+    author: "Daniel Tiger",
+    quote: "When you feel so mad that you want to roar, take a deep breath, and count to four."
+  },
+  {
+    author: "Daniel Tiger",
+    quote: "Saying I’m sorry is the first step, then, how can I help?"
   }
 ];
-var quote = '';
-function getRandomQuote(quotes) {
-  quoteUsed = Math.floor(Math.random() * (quotes.length - 0) + 0); 
-  quote = quotes[quoteUsed].quote + ' - ' + quotes[quoteUsed].author;
+function getRandomQuote() {
+  GLOBAL.quoteUsed = Math.floor(Math.random() * (GLOBAL.quotes.length - 0) + 0);
+  var quote = {
+    text: GLOBAL.quotes[GLOBAL.quoteUsed].quote,
+    author: GLOBAL.quotes[GLOBAL.quoteUsed].author
+  };
+  GLOBAL.quote = quote.text;
   return quote;
 }
-$(function() {
-  $('#quote').text(getRandomQuote(quotes));
-  $('#newquote').click(function() {
-    lastQuote = quoteUsed.toString();
-    $('#quote').text(getRandomQuote(quotes));
+$(function () {
+  var loadedQuote = getRandomQuote();
+  $('#text').text(loadedQuote.text);
+  $('#author').text(loadedQuote.author);
+  $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hastags=quotes&related=freecodecamp&text=' + loadedQuote.text);
+  $('#newquote').click(function () {
+    lastQuote = GLOBAL.quoteUsed.toString();
+    var newQuote = getRandomQuote();
+    //$('#text').text(getRandomQuote(quotes));
+    $('#text').text(newQuote.text);
+    $('#author').text(newQuote.author);
+    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hastags=quotes&related=freecodecamp&text=' + newQuote.text);
   });
-  $('#tweet').click(function(){
+  /*$('#tweet').click(function () {
     var hrefForTweeting = 'https://twitter.com/intent/tweet?hastags=quotes&related=freecodecamp&text=';
-    window.open(hrefForTweeting + quote);
-  });
+    window.open(hrefForTweeting + GLOBAL.quote);
+  });*/
 })
